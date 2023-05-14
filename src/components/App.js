9d35a817f74534e73bb54649b69334b7613cad65
ayushdubey7799 <ayushdubey7799@gmail.com>
2023-05-14 15:17:03 +0530

    completed UI of calculator

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,27 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import { useState } from "react";
+import '../styles/App.css'
 
+const content = ['C','/','*','-','7','8','9','+','4','5','6','1','2','3','=','0','.',' '];
+
+
+
+const App = () => {
+    const [stringToCalculate,setStringToCalculate] = useState("");
+  
+    function calculate(c){
+        setStringToCalculate(stringToCalculate + c);
+    }
+    return (
+        <div id="container">
+            <input type="text"  value={stringToCalculate} readOnly/><br/>
+            <div className="grid">
+            {content.map((item,index) => {
+                return <button className={"item" + index} onClick={() => calculate(item)}>{item}</button>
+            })}
+            </div>
+        </div>
+    )
+}
+
+export default App;
