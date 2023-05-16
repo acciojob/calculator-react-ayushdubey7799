c119c0fc9dbbd818316956545f437ba62cd7273b
ayushdubey7799 <ayushdubey7799@gmail.com>
2023-05-16 12:52:42 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,40 @@ import React from "react";
 import { useState } from "react";
 import '../styles/App.css'
 
-const content = ['C','/','*','-','7','8','9','+','4','5','6','1','2','3','=','0','.',' '];
+const content = ['C', '/', '*', '-', '7', '8', '9', '+', '4', '5', '6', '1', '2', '3', '=', '0', '.', ' '];
 
 
 
 const App = () => {
-    const [stringToCalculate,setStringToCalculate] = useState("");
-  
-    function calculate(c){
-        setStringToCalculate(stringToCalculate + c);
+    const [stringToCalculate, setStringToCalculate] = useState("");
+
+    function calculate(value) {
+        if(value==='='){
+          setStringToCalculate(String(eval(stringToCalculate)));
+        }
+        else if(value === 'C'){
+          setStringToCalculate("");
+        }
+        else{
+            setStringToCalculate(stringToCalculate + value);
+
+        }
     }
+
+    
+
     return (
-        <div id="container">
-            <input type="text"  value={stringToCalculate} readOnly/><br/>
-            <div className="grid">
-            {content.map((item,index) => {
-                return <button className={"item" + index} onClick={() => calculate(item)}>{item}</button>
-            })}
+        <React.Fragment>
+            <h1>React Calculator</h1>
+            <div className="calculator">
+                <input type="text" value={stringToCalculate} /><br />
+                <div className="grid">
+                    {content.map((item, index) => {
+                        return <button key = {index} id = 'btn-9' className={"item" + index} onClick={() => calculate(item)}>{item}</button>
+                    })}
+                </div>
             </div>
-        </div>
+        </React.Fragment>
     )
 }
 
